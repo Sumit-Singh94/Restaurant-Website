a93c7e1d1e74d6f6d07a4423a0d7d12d6d85ec96
Sumit-Singh94 <sumitsin3122@gmail.com>
2024-04-29 09:53:32 +0530

    updated

--- menu/menu.js
+++ menu/menu.js
@@ -1,2 +1,8 @@
 const offcanvasElementList = document.querySelectorAll('.offcanvas')
 const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
+
+
+
+ const title=document.getElementsByClassName('card-title')
+ const text=document.getElementsByClassName('card-text')
+ const price=document.getElementsByClassName('itemprice')
