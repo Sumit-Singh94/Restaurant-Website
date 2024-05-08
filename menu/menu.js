d067db0e3d475f68a9927c4b6912adb5ed1196cd
Sumit-Singh94 <sumitsin3122@gmail.com>
2024-05-08 22:39:16 +0530

    menu updated

--- menu/menu.js
+++ menu/menu.js
@@ -1,8 +1,28 @@
-const offcanvasElementList = document.querySelectorAll('.offcanvas')
-const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
+cart=[]
+cartbtn=document.querySelectorAll('#cartbtn')
 
+cartbtn.forEach(button => {
+    button.addEventListener('click',function(e){
+        e.preventDefault()
 
+       card= button.parentElement.parentElement
 
- const title=document.getElementsByClassName('card-title')
- const text=document.getElementsByClassName('card-text')
- const price=document.getElementsByClassName('itemprice')
+       let product={
+        productTitle:card.querySelectorAll('.card-title').innerHTML,
+        productText:card.querySelectorAll('.card-text').innerText,
+        productPrice:parseFloat(card.querySelectorAll('.itemprice').innerText),
+        quantity: 1
+    
+       }
+       cart.push(product)
+
+       canvasbody=document.querySelector('.offcanvas-body')
+
+       cartItem=document.createElement('div');
+       cartItem.textContent=`${product.productTitle,product.productText,product.productPrice*product.quantity}`
+
+       canvasbody.appendChild(cartItem)
+       console.log(cart);
+
+    })
+});
