0b768a4e6bb8acba8e7049bf14582665bb294417
Sumit-Singh94 <sumitsin3122@gmail.com>
2024-07-21 10:30:51 +0530

    done with adding form

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,6 +1,21 @@
+loginForm=document.getElementById("loginForm")
+loginForm.style.display="none";
+
+
+let formTime=setTimeout(()=>{
+   loginForm.style.display="block";
+   console.log("diplayed!");
+},5000);
+
+setTimeout(()=>{
+   loginForm.style.display="none";
+   clearTimeout(formTime)
+
+},10000)
+
 arrow=document.querySelector(".arrow")
-
-
 arrow.addEventListener('click',()=>{
    window.scrollBy(0,500)  
 })
+
+
