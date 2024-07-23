280ac467dbea1b862ca9405a0fe5a3c49336e769
Sumit-Singh94 <sumitsin3122@gmail.com>
2024-07-23 23:19:00 +0530

    finally completed

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -4,16 +4,36 @@ loginForm.style.display="none";
 
 let formTime=setTimeout(()=>{
    loginForm.style.display="inline-block"
+  loginForm.style.position="fixed";
    console.log("LoginFormDisplayed!!");
-},7000);
+},3000);
 
 cancel=document.getElementById('cancel')
-
 cancel.addEventListener('click',event=>{
    loginForm.style.display="none";
    clearTimeout(formTime)
+   formTime=null;
    console.log("LoginFormRemoved!!");
+   
 })
+
+
+function signinbtn() {
+    const signbtn=document.querySelector('.sign')
+    signbtn.addEventListener('click',(event)=>{
+    event.preventDefault()
+      loginForm.style.display="none";
+      if(formTime!=null){
+         clearTimeout(formTime)
+         formTime=null;
+         console.log("LoginFormRemoved!!");
+      }
+     
+    })
+}
+signinbtn()
+
+
 
 
 arrow=document.querySelector(".arrow")
